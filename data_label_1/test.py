56a229a89cc0ef29d8cf94380f176435aae9d3ca
chenfancc <3191937105@qq.com>
2024-05-15 00:40:33 +0800

    1.0

--- data_label_1/test.py
+++ data_label_1/test.py
@@ -5,6 +5,7 @@
 #     run_step2_label_1()
 #     run_step3_label_1()
 
+
 if __name__ == '__main__':
     from step5_label_1 import run_step5_label_1
     from step6_label_1 import run_step6_label_1
